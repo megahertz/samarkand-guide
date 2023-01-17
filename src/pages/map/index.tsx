fb9464c80c940b014aa96a1d5935e1fc71198f78
Alexey Prokhorov <megahertz@l96.ru>
2023-01-18 03:33:07 +0500

    feat(map): Highlight active menu item

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -7,7 +7,7 @@ import { placemarkMatchesUrl } from '@site/src/pages/map/lib/helpers';
 import DocSidebar from '@theme/DocSidebar';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
-import React from 'react';
+import React, { useEffect } from 'react';
 import FullMap from './components/FullMap';
 import styles from './styles.module.css';
 
@@ -16,6 +16,7 @@ const placemarks = getPlacemarkItems();
 export default function Map() {
   const { siteConfig } = useDocusaurusContext();
   const location = useLocation();
+  useMenuHighlighting();
 
   const sidebarItems = getSidebarItems(location.hash);
 
@@ -48,4 +49,32 @@ export default function Map() {
       </main>
     </Layout>
   );
+}
+
+function useMenuHighlighting() {
+  const ACTIVE_CLASS = 'menu__link--active';
+  const location = useLocation();
+
+  useEffect(() => {
+    const $links = document.querySelectorAll('.menu__link');
+    const $menu = document.querySelector('.menu');
+
+    $menu?.addEventListener('click', onMenuClick);
+    $links.forEach(($link) => {
+      if ($link.getAttribute('href') === location.hash) {
+        $link.classList.add(ACTIVE_CLASS);
+      }
+    });
+
+    function onMenuClick(e) {
+      document
+        .querySelectorAll('.menu__link')
+        .forEach((l) => l.classList.remove(ACTIVE_CLASS));
+      e.target.classList?.add(ACTIVE_CLASS);
+    }
+
+    return () => {
+      $menu?.removeEventListener('click', onMenuClick);
+    };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 }
