9f98bf7d450ef8012748d6c181836bccdc0a3ba6
Alexey Prokhorov <megahertz@l96.ru>
2023-02-23 22:41:31 +0500

    chore(utilities): Add a few details

--- map/city/utilities.ts
+++ map/city/utilities.ts
@@ -13,7 +13,10 @@ export default {
       yandexMap: 'https://yandex.uz/maps/org/224799588118/',
       googleMap:
         'https://www.google.com/maps/place/Samarqand+Shaxar+Elektroset/@39.6515348,66.9518127,18.16z/data=!4m6!3m5!1s0x3f4d19723f6eb367:0xfb74454ee208261f!8m2!3d39.6521422!4d66.9515466!16s%2Fg%2F11h6ptdkp5',
+      web: 'https://het.uz/ru',
+      telegram: 'https://t.me/HET_admin',
       phones: [
+        '1154',
         '+998 (66) 233-12-12',
         '+998 (66) 233-12-15',
         '+998 (66) 233-41-79',
