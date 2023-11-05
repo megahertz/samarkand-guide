b507b9622f2d16f14e3ad88ee4e1ac2f9a46b9b7
Alexey Prokhorov <megahertz@l96.ru>
2023-11-05 16:39:27 +0500

    chore: Add more bazar info

--- map/market/bazar.ts
+++ map/market/bazar.ts
@@ -9,6 +9,7 @@ export default {
       label: 'Сиабский базар',
       location: [39.662401, 66.980703],
       yandexMap: 'https://yandex.uz/maps/org/229237297205/',
+      twoGis: 'https://2gis.uz/samarkand/firm/70000001079119113',
       googleMap: 'https://goo.gl/maps/8WWsPwoKN7QnkqQ67',
       description: 'Крупнейший рынок. Не работает по понедельникам.',
     },
@@ -16,16 +17,20 @@ export default {
       label: 'Железнодорожный рынок',
       location: [39.682124, 66.930143],
       yandexMap: 'https://yandex.uz/maps/org/235666538486/',
+      twoGis: 'https://2gis.uz/samarkand/firm/70000001077171883',
       googleMap: 'https://goo.gl/maps/W57fkE9kMsMnjZEa8',
       description:
         'Второй по величине рынок. Много хозмагов. Самые интересные цены у ' +
         'продавцов в ближайшем к Беруни ряду, а так-же у тех кто за ' +
-        'пределами рынка, на самой улице Беруни.',
+        'пределами рынка, на самой улице Беруни. Еще интересные цены ' +
+        'бывают до 9 утра, когда приезжают сами фермеры торговать. По ' +
+        'выходным цены могут быть чуть дороже.',
     },
     {
       label: 'ДСК Базар (Damariq)',
       location: [39.664215, 66.910525],
       yandexMap: 'https://yandex.uz/maps/org/216933972161/',
+      twoGis: 'https://2gis.uz/samarkand/firm/70000001078865146',
       description:
         'Рано утром фермеры привозят сюда свою продукцию и продают недорого. ' +
         'Также рынок примечателен большим количеством цветов и саженцев.',
@@ -35,6 +40,7 @@ export default {
         'Базар на Дальнем Лагере (Hazora/Muso Xonkeldiyev nomli buyum bozori)',
       location: [39.687412, 66.896273],
       yandexMap: 'https://yandex.uz/maps/org/104467559050/',
+      twoGis: 'https://2gis.uz/samarkand/firm/70000001080056010',
       googleMap: 'https://goo.gl/maps/pFj2E472B89JYCX96',
       video: 'lpsDaI6a1M0',
       description:
@@ -45,6 +51,7 @@ export default {
       label: 'Янги Базар',
       location: [39.662713, 66.996225],
       yandexMap: 'https://yandex.com/web-maps/-/CDQgjL4B',
+      twoGis: 'https://2gis.uz/samarkand/firm/70000001079115788',
       googleMap: 'https://goo.gl/maps/TSZpsFb7UFxhTJzX6',
       description: 'Крупнейший вещевой рынок.',
     },
@@ -52,6 +59,7 @@ export default {
       label: 'Первая остановка микрорайона',
       location: [39.661484, 66.921376],
       yandexMap: 'https://yandex.uz/maps/-/CCUzNUdvDD',
+      twoGis: 'https://2gis.uz/samarkand/firm/70000001078940042',
       description:
         'Небольшой продуктово-вещевой рынок. Специально ехать нет смысла, ' +
         'только если живете поблизости. Хотя, кто-то сюда специально за ' +
@@ -61,6 +69,7 @@ export default {
       label: 'Вещевой рынок около вокзала',
       location: [39.68542, 66.926635],
       yandexMap: 'https://yandex.uz/maps/-/CCUzNUHecC',
+      twoGis: 'https://2gis.uz/samarkand/firm/70000001080305287',
       description:
         'Не путать с железнодорожным. Он находится слева от вокзала. Недорогая одежда, особенно детская.',
     },
@@ -69,6 +78,7 @@ export default {
       location: [39.655012, 66.958813],
       yandexMap:
         'https://yandex.uz/maps/org/kryty_rynok_bazar_bozori_central_market/191042139990/',
+      twoGis: 'https://2gis.uz/samarkand/search/firm/70000001080940014',
       description:
         'Небольшой рынок в центре города. Цены средние. Можно поменять валюту.',
     },
