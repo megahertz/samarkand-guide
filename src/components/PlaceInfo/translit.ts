4a937990fb1824e0879cf710d866050b06f43e5c
Alexey Prokhorov <megahertz@l96.ru>
2024-01-05 21:54:45 +0500

    chore: lint

--- src/components/PlaceInfo/translit.ts
+++ src/components/PlaceInfo/translit.ts
@@ -1,39 +1,39 @@
 /* eslint-disable prettier/prettier */
 
 const MAP: Record<string, string> = {
-  'а': 'a',
-  'б': 'b',
-  'в': 'v',
-  'г': 'g',
-  'д': 'd',
-  'е': 'ye',
-  'ё': 'yo',
-  'ж': 'zh',
-  'з': 'z',
-  'и': 'i',
-  'й': 'y',
-  'к': 'k',
-  'л': 'l',
-  'м': 'm',
-  'н': 'n',
-  'о': 'o',
-  'п': 'p',
-  'р': 'r',
-  'с': 's',
-  'т': 't',
-  'у': 'u',
-  'ф': 'f',
-  'х': 'kh',
-  'ц': 'ts',
-  'ч': 'ch',
-  'ш': 'sh',
-  'щ': 'shch',
-  'ъ': '',
-  'ы': 'i',
-  'ь': '',
-  'э': 'e',
-  'ю': 'yu',
-  'я': 'ya',
+  а: 'a',
+  б: 'b',
+  в: 'v',
+  г: 'g',
+  д: 'd',
+  е: 'ye',
+  ё: 'yo',
+  ж: 'zh',
+  з: 'z',
+  и: 'i',
+  й: 'y',
+  к: 'k',
+  л: 'l',
+  м: 'm',
+  н: 'n',
+  о: 'o',
+  п: 'p',
+  р: 'r',
+  с: 's',
+  т: 't',
+  у: 'u',
+  ф: 'f',
+  х: 'kh',
+  ц: 'ts',
+  ч: 'ch',
+  ш: 'sh',
+  щ: 'shch',
+  ъ: '',
+  ы: 'i',
+  ь: '',
+  э: 'e',
+  ю: 'yu',
+  я: 'ya',
 };
 
 export default function translit(str: string): string {
@@ -42,7 +42,7 @@ export default function translit(str: string): string {
     .map((char) => {
       const loweredChar = char.toLowerCase();
 
-      const converted = MAP[loweredChar] || char
+      const converted = MAP[loweredChar] || char;
 
       if (char === loweredChar) {
         return converted.toLowerCase();
