01ddb5dcff9ca0d1a1125ace60d53d8852ef9291
Alexey Prokhorov <megahertz@l96.ru>
2024-01-05 21:59:41 +0500

    chore: lint

--- src/components/PlaceInfo/translit.ts
+++ src/components/PlaceInfo/translit.ts
@@ -1,41 +1,13 @@
 /* eslint-disable prettier/prettier */
-
 // prettier-ignore
 const MAP: Record<string, string> = {
-  'а': 'a',
-  'б': 'b',
-  'в': 'v',
-  'г': 'g',
-  'д': 'd',
-  'е': 'ye',
-  'ё': 'yo',
-  'ж': 'zh',
-  'з': 'z',
-  'и': 'i',
-  'й': 'y',
-  'к': 'k',
-  'л': 'l',
-  'м': 'm',
-  'н': 'n',
-  'о': 'o',
-  'п': 'p',
-  'р': 'r',
-  'с': 's',
-  'т': 't',
-  'у': 'u',
-  'ф': 'f',
-  'х': 'kh',
-  'ц': 'ts',
-  'ч': 'ch',
-  'ш': 'sh',
-  'щ': 'shch',
-  'ъ': '',
-  'ы': 'i',
-  'ь': '',
-  'э': 'e',
-  'ю': 'yu',
-  'я': 'ya',
+  'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'ye', 'ё': 'yo',
+  'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm',
+  'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+  'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ъ': '',
+  'ы': 'i', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya',
 };
+/* eslint-enable prettier/prettier */
 
 export default function translit(str: string): string {
   return str
